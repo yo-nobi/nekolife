594eedecc93890f5819c70c1545113293f46ae14
takayuki.suzuki <babibubebown1018@gmail.com>
2020-09-23 11:33:10 +0000

    nekolife2

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'kaminari'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,5 @@
 class MicropostsController < ApplicationController
   
-  def index
-  end
-  
   def index_food
     @food = Micropost.new
     @foods = Micropost.where(category: "食事").order(id: :desc).page(params[:page]).per(5)
@@ -31,24 +28,27 @@ class MicropostsController < ApplicationController
       flash[:success] = '質問を投稿しました。'
       if (@micropost.category == "食事")
         @foods = Micropost.where(category: "食事").order(id: :desc).page(params[:page]).per(5)
-        redirect_to foods
+        redirect_to food_url
       elsif (@micropost.category == "棲み家")
         @lives = Micropost.where(category: "棲み家").order(id: :desc).page(params[:page]).per(5)
-        redirect_to lives
+        redirect_to live_url
       else (@micropost.category == "遊び")
         @plays = Micropost.where(category: "遊び").order(id: :desc).page(params[:page]).per(5)
-        redirect_to plays
+        redirect_to play_url
       end
     else
       @microposts = Micropost.order(id: :desc).page(params[:page])
       flash.now[:danger] = '質問の投稿に失敗しました。'
       if (@micropost.category == "食事")
+        @food = @micropost
         @foods = Micropost.where(category: "食事").order(id: :desc).page(params[:page]).per(5)
         render "index_food"
       elsif (@micropost.category == "棲み家")
+        @live = @micropost
         @lives = Micropost.where(category: "棲み家").order(id: :desc).page(params[:page]).per(5)
         render "index_live"
       else (@micropost.category == "遊び")
+        @play = @micropost
         @plays = Micropost.where(category: "遊び").order(id: :desc).page(params[:page]).per(5)
         render "index_play"
       end
